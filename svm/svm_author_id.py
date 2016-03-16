f4933ae183cc96224e1f8a7b60752d60f1ea073d
Hemendra <hemendra26@gmail.com>
2016-03-16 06:14:17 +0530

    added svm code

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -13,12 +13,21 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+from sklearn.svm import SVC
+from time import time
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = SVC(kernel="linear")
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
+print clf.score(features_test, labels_test)
+print "testing time:", round(time()-t1, 3), "s"
 
 
 
